2d6482a61bcc65f277cfec844f1484fe6e679e2e
mejdja <meetya@freenet.de>
2020-08-30 13:15:07 +0200

    complete bfs termination proof

--- Queue_Specs.thy
+++ Queue_Specs.thy
@@ -9,24 +9,6 @@ locale Queue =
   fixes head :: "'q \<Rightarrow> 'a"
   fixes tail :: "'q \<Rightarrow> 'q"
   fixes invar :: "'q \<Rightarrow> bool"
-  fixes list :: "'q \<Rightarrow> 'a list"
-  assumes list_empty: "list empty = Nil"
-  assumes is_empty: "invar q \<Longrightarrow> is_empty q = (list q = Nil)"
-  assumes list_snoc: "invar q \<Longrightarrow> list (snoc q x) = list q @ [x]"
-  assumes list_head: "\<lbrakk> invar q; list q \<noteq> Nil \<rbrakk> \<Longrightarrow> head q = hd (list q)"
-  assumes list_tail: "\<lbrakk> invar q; list q \<noteq> Nil \<rbrakk> \<Longrightarrow> list (tail q) = tl (list q)"
-  assumes invar_empty: "invar empty"
-  assumes invar_snoc: "invar q \<Longrightarrow> invar (snoc q x)"
-  assumes invar_tail: "\<lbrakk> invar q; list q \<noteq> Nil \<rbrakk> \<Longrightarrow> invar (tail q)"
-
-(*
-locale Queue =
-  fixes empty :: "'q"
-  fixes is_empty :: "'q \<Rightarrow> bool"
-  fixes snoc :: "'q \<Rightarrow> 'a \<Rightarrow> 'q"
-  fixes head :: "'q \<Rightarrow> 'a"
-  fixes tail :: "'q \<Rightarrow> 'q"
-  fixes invar :: "'q \<Rightarrow> bool"
   fixes mset :: "'q \<Rightarrow> 'a multiset"
   assumes mset_empty: "mset empty = {#}"
   assumes is_empty: "invar q \<Longrightarrow> is_empty q = (mset q = {#})"
@@ -36,6 +18,5 @@ locale Queue =
   assumes invar_empty: "invar empty"
   assumes invar_snoc: "invar q \<Longrightarrow> invar (snoc q x)"
   assumes invar_tail: "\<lbrakk> invar q; mset q \<noteq> {#} \<rbrakk> \<Longrightarrow> invar (tail q)"
-*)
 
 end
